32665fe926fa3104f4a524f13afe2d76b040e0d1
ygorxds <ygordesa2010@hotmail.com>
2023-05-26 21:31:39 -0300

    changes document

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openWhatsApp() {
+    // Substitua "seu_numero" pelo número de telefone para o qual deseja enviar a mensagem
+    var phoneNumber = "5521978788006";
+    
+    // Abra o link do WhatsApp com o número de telefone e uma mensagem predefinida
+    window.open("https://wa.me/" + phoneNumber + "?text=Olá, gostaria de entrar em contato!", "_blank");
+}
