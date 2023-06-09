26d8a58afd44a8e36a62c38734ae3d50a42dc12e
ygorxds <ygordesa2010@hotmail.com>
2023-06-08 21:05:20 -0300

    create: tratament page

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ function openWhatsApp() {
     // Abra o link do WhatsApp com o número de telefone e uma mensagem predefinida
     window.open("https://wa.me/" + phoneNumber + "?text=Olá, gostaria de entrar em contato!", "_blank");
 }
+
+var slides = document.getElementsByClassName('custom-slide');
+var currentSlide = 0;
+
+function showSlide(n) {
+  slides[currentSlide].classList.remove('custom-active');
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].classList.add('custom-active');
+}
+
+showSlide(0); // Show the first slide initially
+
+// Add event listeners for arrow clicks
+document.getElementById('nextBtn').addEventListener('click', function() {
+  showSlide(currentSlide + 1);
+});
+
+document.getElementById('prevBtn').addEventListener('click', function() {
+  showSlide(currentSlide - 1);
+});
